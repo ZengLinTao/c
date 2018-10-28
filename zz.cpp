a8bb2fe7a2f6fcf53dca544f12577ad136f62843
DataStruct <40025730+ZengLinTao@users.noreply.github.com>
2018-10-28 17:12:40 +0800

    排序算法

--- zz.cpp
+++ zz.cpp
@@ -6,6 +6,8 @@
 #include"stdlib.h"
 using namespace std;
 void Maopao(int a[]) {
+
+	long long start = clock();
 	int t;
 	for (int i = 0; i < 5000 - 1; i++) {
 		for (int j = 0; j < 5000 - 1 - i; j++)
@@ -18,10 +20,15 @@ void Maopao(int a[]) {
 		}
 	}
 	for (int i = 0; i < 5000; i++)
-		cout << a[i] << " ";
+		cout << a[i] << "  ";
 	cout << endl;
+	long long end = clock();
+	cout << "\n时间为\n" << endl;
+	cout << double(end - start) / CLOCKS_PER_SEC << endl;
+
 }
 void Choose(int a[]) {
+	long long start = clock();
 	int mix, temp;
 	for (int i = 0; i < 5000; i++) {
 		mix = i;
@@ -35,11 +42,15 @@ void Choose(int a[]) {
 		}
 	}
 	for (int i = 0; i < 5000; i++)
-		cout << a[i] << " ";
+		cout << a[i] << "  ";
 	cout << endl;
+	long long end = clock();
+	cout << "\n时间为\n" << endl;
+	cout << double(end - start) / CLOCKS_PER_SEC << endl;
 
 }
 void insert(int a[]){
+	long long start = clock();
 	int i, j, k  ;
 
 	   for (i = 1; i < 5000; i++)
@@ -61,10 +72,41 @@ void insert(int a[]){
 			        }
 		    }
 	   for (int i = 0; i < 5000; i++)
-		  cout << a[i] << " ";
+		  cout << a[i] << "  ";
 	   cout << endl;
-
+	   long long end = clock();
+	   cout << "\n时间为\n" << endl;
+	   cout << double(end - start) / CLOCKS_PER_SEC << endl;
 }
+
+void shellsort(int a[]) {
+	long long start = clock();
+	int i, j;
+	int r, temp;
+	int x = 0;
+	for (r = 5000 / 2; r >= 1; r /= 2)//r>=1保证至少有两个可以比较的数，r/=2确定序列对
+	{
+		for (i = r; i < 5000; i++)
+		{
+			temp = a[i];
+			j = i - r;
+			while (j >= 0 && temp < a[j])//对配对的两个数进行比较
+			{
+				a[j + r] = a[j];
+				j -= r;
+			}
+			a[j + r] = temp;
+		}
+	}
+	for (int i = 0; i < 5000; i++)
+		cout << a[i] << "  ";
+	cout << endl;
+	long long end = clock();
+	cout << "\n时间为\n" << endl;
+	cout << double(end - start) / CLOCKS_PER_SEC << endl;
+}
+	
+
 
 int main(){
   ofstream out;
@@ -78,32 +120,24 @@ int main(){
   int a[5000];
   int i;
   int *p;
-  for (int i = 0; i < 5000; i++)
+  for ( i = 0; i < 5000; i++)
   {
-	  a[i] = rand() % 100000;
-	  
-	 
+	  a[i] = rand() % 100000;	 
   }
   p = a;
    for (int i=0;i<5000;i++)  
 	  in.close();
-  long long start =clock();
-  int ch;
+   int ch;
   while (1) {
-	  printf("请选择排序算法 \n 1：冒泡排序\n 2: 选择排序 \n 3：插入排序 \n 4:shell排序 \n 5：归并排序 \n 6：快速排序 \n ");
+	  printf("请选择排序算法 \n 1：冒泡排序\n 2: 选择排序 \n 3：插入排序 \n 4:shell排序 \n \n \n");
 	  scanf_s("%d", &ch);
 	  switch (ch) {
-	  case 1:  Maopao(a);
-	  case 2:  Choose(a);
-	  case 3:  insert(a);
+	  case 1:  Maopao(a); continue;
+	  case 2:  Choose(a); continue;
+	  case 3:  insert(a); continue;
+	  case 4:  shellsort(a); continue;
 	  }
-
   }
-  
-
-  long long end=clock();
-  cout<<double(end-start)/CLOCKS_PER_SEC<<endl;
-
   system("pause");
   return 0;
 
